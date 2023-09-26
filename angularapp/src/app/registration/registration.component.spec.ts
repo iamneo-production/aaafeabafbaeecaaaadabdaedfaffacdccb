1131e0d647e88f44f46ed3a76c1ce33b7e7bc2ad
Iamneo <techadmin@iamneo.ai>
2023-09-26 18:18:52 +0000

    Backup Commit

--- angularapp/src/app/registration/registration.component.spec.ts
+++ angularapp/src/app/registration/registration.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { RegistrationComponent } from './registration.component';
 
 describe('RegistrationComponent', () => {
@@ -8,9 +8,9 @@ describe('RegistrationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegistrationComponent ]
-    })
-    .compileComponents();
+      declarations: [RegistrationComponent],
+      imports: [ReactiveFormsModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +19,29 @@ describe('RegistrationComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the registration component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create a form with initial invalid state', () => {
+    expect(component.registrationForm.valid).toBeFalsy();
+  });
+
+  it('should require First Name', () => {
+    const firstName = component.registrationForm.controls['firstName'];
+    firstName.setValue('');
+    expect(firstName.valid).toBeFalsy();
+  });
+
+  it('should require Last Name', () => {
+    const lastName = component.registrationForm.controls['lastName'];
+    lastName.setValue('');
+    expect(lastName.valid).toBeFalsy();
+  });
+
+  it('should require a valid Email', () => {
+    const email = component.registrationForm.controls['email'];
+    email.setValue('invalid-email');
+    expect(email.valid).toBeFalsy();
   });
 });
