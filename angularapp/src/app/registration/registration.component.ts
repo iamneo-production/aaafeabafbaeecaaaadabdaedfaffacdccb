d06f8a910a26607896c6a938e7da2854257ad9c5
Iamneo <techadmin@iamneo.ai>
2023-09-26 17:43:52 +0000

    Backup Commit

--- angularapp/src/app/registration/registration.component.ts
+++ angularapp/src/app/registration/registration.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
 })
-export class RegistrationComponent implements OnInit {
+export class RegistrationComponent {
+  registrationForm: FormGroup;
+  registrationSuccess = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required],
+    });
   }
 
+  passwordsMatch(): boolean {
+    const password = this.registrationForm.get('password').value;
+    const confirmPassword = this.registrationForm.get('confirmPassword').value;
+    return password === confirmPassword;
+  }
+
+  onSubmit(): void {
+    if (this.registrationForm.valid && this.passwordsMatch()) {
+      // Register the user or perform desired actions here
+      this.registrationSuccess = true;
+    }
+  }
 }
